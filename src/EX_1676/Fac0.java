4811442ccef20de82df3ba22a5b9ca980ccabd27
lovelyborie <upuforever@kau.kr>
2021-11-20 18:51:46 +0900

    팩토리얼 0의개수

--- src/EX_1676/Fac0.java
+++ src/EX_1676/Fac0.java
@@ -1,5 +1,16 @@
 package EX_1676;
 
+import java.util.Scanner;
 public class Fac0 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		int zeronum = 0;
+		while(input >=5) {
+			zeronum += input/5;
+			input /=5;
+		}
+		System.out.println(zeronum);
+	}
 
 }
