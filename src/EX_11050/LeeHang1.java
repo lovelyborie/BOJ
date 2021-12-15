90fd370c1e722ba59f082e60fd2ab44d928f1932
lovelyborie <upuforever@kau.kr>
2021-12-16 00:57:01 +0900

    이항계수1

--- src/EX_11050/LeeHang1.java
+++ src/EX_11050/LeeHang1.java
@@ -1,5 +1,19 @@
 package EX_11050;
-
+import java.util.Scanner;
 public class LeeHang1 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int K = scan.nextInt();
+		System.out.println(LeeHang(N,K));
+	}
+	public static int LeeHang(int N , int K) {
+		return fact(N)/(fact(K)*fact(N-K));
+	}
+	public static int fact(int N) {
+		if(N<=1)
+			return 1;
+		else
+			return fact(N-1)*N;
+	}
 }
