5f1fdd9f674830eed9cd0a83c7d236b7565b1346
lovelyborie <upuforever@kau.kr>
2022-01-01 16:16:23 +0900

    최대값

--- src/EX_2562/Max_Value.java
+++ src/EX_2562/Max_Value.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Max_Value {
 	public static void main (String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] nums = new int[9];
+		int[] nums = new int[10];
 		int Max = -1; int Max_index = -1;
-		for (int i=0 ; i<9 ; i++) 
+		for (int i=1 ; i<10 ; i++) 
 			nums[i] = scan.nextInt();
-		for (int j=0; j<9 ; j++) {
+		for (int j=1; j<10 ; j++) {
 			if (nums[j] > Max) {
 				Max = nums[j];
-				Max_index = j+1;
+				Max_index = j;
 			}
 		}
 		System.out.println(Max);
